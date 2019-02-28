cbc67f0684394dd916bb963df72d69dc11d1374a
Samuel Sousa <samuel.sousa@gmail.com>
2019-02-28 15:36:41 +0000

    add angular functionality like model and get item dynamically.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model} from './model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  model = new Model();
+
+  getName() {
+    return this.model.user;
+  }
+
+  getTodoItems() {
+    return this.model.items;
+  }
 }
